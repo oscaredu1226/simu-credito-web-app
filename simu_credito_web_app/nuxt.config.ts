3339b54054654838821ba6ede220f7b73a56cf9a
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-28 20:00:39 -0500

    chore: add api route

--- simu_credito_web_app/nuxt.config.ts
+++ simu_credito_web_app/nuxt.config.ts
@@ -1,14 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      apiBase: 'https://simu-credito-platform-6n2e.onrender.com/api'
-    }
-  },
-  css: ['~/assets/css/main.css'],
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    ssr: false,
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    runtimeConfig: {
+        public: {
+            apiBase: 'https://simu-credito-platform-6n2e.onrender.com/api'
+        }
+    },
+    css: ['~/assets/css/main.css'],
+    modules: ['@nuxtjs/tailwindcss']
 })
